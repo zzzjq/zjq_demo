e08f9e36ecc588f691c526c69243e52f634becf8
zhangjingqi <zhangjingqiyy@sina.com>
2019-02-15 17:54:16 +0800

    interrupt学习

--- zjq.site/src/test/java/zjq/test/thread/InterruptTest.java
+++ zjq.site/src/test/java/zjq/test/thread/InterruptTest.java
@@ -1,26 +1,37 @@
 package zjq.test.thread;
 
-public class InterruptTest extends Object implements Runnable {
+/**
+ * interrupt只是给线程设置一个中断标志位置，对正在运行的线程没有任何影响。
+ * 只有当线程在sleep、wait、join的时候，
+ * 如果被interrupt，就会抛出InterruptedException，表示被中断当前的状态，
+ * 线程可以catch后，做操作或者直接结束线程。
+ * 这个东西类似于一个信号量，告知该线程，线程可以通过catch InterruptedException 或者
+ * 判断isInterrupted，来做下一步操作。
+ * 
+ * @author idealife
+ *
+ */
+public class InterruptTest implements Runnable {
 
+	@Override
 	public void run (){
 		try{
-			System.out.println("start");
-			Thread.sleep(2000);
-			System.out.println("wakeup");
-			Thread.interrupted();
+			System.out.println("线程开始");
+			System.out.println("当前线程是否中断状态:" + Thread.currentThread().isInterrupted());
+			Thread.sleep(5000);
+			System.out.println("线程结束sleep");
 		}catch(InterruptedException e){
-			System.out.println("interrupted");
+			System.out.println("当前线程是否中断状态:" + Thread.currentThread().isInterrupted());
 			return;
 		}
-		System.out.println("leave");
+		System.out.println("当前线程是否中断状态:" + Thread.currentThread().isInterrupted());
+		System.out.println("线程死亡");
 	}
 	
 	public static void main(String[] args){
-		InterruptTest a = new InterruptTest();
-		Thread t = new Thread(a);
+		Thread t = new Thread(new InterruptTest());
 		t.start();
-		System.out.println("interrupted other");
+		System.out.println("中断线程t");
 		t.interrupt();
-		System.out.println("leaving");
 	}
 }
