f9fc6937eaaa84ef967e165fa62481a072848d97
zhangjingqi <zhangjingqiyy@sina.com>
2019-05-27 18:29:24 +0800

    斐波那契数列

--- zjq.site/src/main/java/zjq/demo/service/aopservice/AnotionAopService.java
+++ zjq.site/src/main/java/zjq/demo/service/aopservice/AnotionAopService.java
@@ -2,7 +2,6 @@ package zjq.demo.service.aopservice;
 
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +17,12 @@ public class AnotionAopService {
 
 	@Pointcut("execution(* zjq.demo.service.aopservice.HelloWorldService.*(..))")
 	public void pointcut() {
-		
+
 	}
-	
-	@Before("pointcut()")
+
+	@Around("pointcut()")
 	public void say() {
 		System.out.println("AnotionAopService aop");
 	}
-	
+
 }
